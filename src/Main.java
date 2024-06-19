d71661d346f861b0e37888cd29c3d959e9a179ce
kotedo <tray202_99@mail.ru>
2024-06-19 19:50:40 +0300

    исправление на класс дата

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.sql.Time;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         task1();
     }
     private static void task1(){
-        System.out.println("2024-10-01");
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
     }
 }
